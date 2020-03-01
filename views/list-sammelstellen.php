98a5cab9af5e9bbc442d2a335d3005036a516573
Christoph Böhme <christoph@b3e.net>
2020-03-01 20:06:30 +0100

    Add link from Sammelstellen list to create Sammelstelle

--- views/list-sammelstellen.php
+++ views/list-sammelstellen.php
@@ -1,3 +1,4 @@
 <div class="wrap">
-    <h1>Sammelstellen</h1>
+    <h1 class="wp-heading-inline">Sammelstellen</h1>
+    <a class="page-title-action" href="<?= esc_url( Sammelstellen_Admin::get_page_url() ); ?>">Neu hinzufügen</a>
 </div>
