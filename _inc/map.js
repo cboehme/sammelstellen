971efe075e67020d6fd6c74c32a8495af5d43929
Christoph Böhme <christoph@b3e.net>
2020-03-16 21:28:44 +0100

    Switch to raster map

--- _inc/map.js
+++ _inc/map.js
@@ -1,32 +1,25 @@
 const view = new ol.View({
     center: ol.proj.fromLonLat([7.1006600, 50.7358510]),
     extent: ol.proj.transformExtent([6.95, 50.60, 7.35, 50.80], 'EPSG:4326', 'EPSG:3857'),
-    zoom: 10
+    minZoom: 12,
+    maxZoom: 20,
+    zoom: 12
 });
 
 const map = new ol.Map({
     layers: [
-/*        new ol.layer.VectorTile({
-            source: new ol.source.VectorTile({
-                format: new ol.format.MVT(),
-                url: 'http://localhost:8080/data/v3/{z}/{x}/{y}.pbf',
-                maxZoom: 18
-            })
-        })
- */
         new ol.layer.Tile({
-            source: new ol.source.OSM({
-                url: 'http://localhost:8080/styles/positron/{z}/{x}/{y}.png',
-                maxZoom: 18
+            source: new ol.source.XYZ({
+                url: 'http://localhost:8080/styles/klokantech-basic/{z}/{x}/{y}.png',
+                minZoom: 12,
+                maxZoom: 20,
+                tilePixelRatio: 2
             })
         })
     ],
     target: 'map',
     view: view
 });
-
-//const map = apply('map', 'http://localhost:8080/styles/positron/style.json');
-//map.setView(view);
 
 const geolocation = new ol.Geolocation({
     trackingOptions: {
@@ -83,44 +76,6 @@ const iconStyle = new ol.style.Style({
         src: 'sammelstelle.svg'
     })
 });
-
-const geoJson = {
-    'type': 'FeatureCollection',
-    'features': [{
-        'type': 'Feature',
-        'geometry': {
-            'type': 'Point',
-            'coordinates': [7.1136841, 50.740083]
-        },
-        'properties': {
-            'name': 'Miss Minz',
-            'adresse': 'Rheinaustraße 101, 53225 Bonn',
-            'website': 'http://www.missminz.de/',
-        }
-    }, {
-        'type': 'Feature',
-        'geometry': {
-            'type': 'Point',
-            'coordinates': [7.1220443, 50.7408999]
-        },
-        'properties': {
-            'name': 'Momo Bioladen',
-            'adresse': 'Hans-Böckler-Straße 1, 53225 Bonn',
-            'website': 'https://bioladen.com/'
-        }
-    }, {
-        'type': 'Feature',
-        'geometry': {
-            'type': 'Point',
-            'coordinates': [7.0963468, 50.7397712]
-        },
-        'properties': {
-            'name': 'ADFC RadHaus',
-            'adresse': 'Breite Straße 71, 53111 Bonn',
-            'website': 'https://adfc-bonn.de/'
-        }
-    }]
-};
 
 var sammelstellen = new ol.layer.Vector({
     map: map,
