a3d717361b6b8e275deddba5fb25d79d3be89f1c
Christoph Böhme <christoph@b3e.net>
2020-03-22 19:39:28 +0100

    Migrate shortcode map to mapbox-gl

--- _inc/map.js
+++ _inc/map.js
@@ -1,118 +1,50 @@
-const view = new ol.View({
-    center: ol.proj.fromLonLat([7.1006600, 50.7358510]),
-    extent: ol.proj.transformExtent([6.95, 50.60, 7.35, 50.80], 'EPSG:4326', 'EPSG:3857'),
-    minZoom: 12,
-    maxZoom: 20,
+const map = new mapboxgl.Map({
+    container: 'map',
+    style: 'http://localhost:8080/styles/positron/style.json',
+    maxBounds: [[6.95, 50.60], [7.35, 50.80]],
+    hash: true,
+    center: [7.1006600, 50.7358510],
     zoom: 12
 });
 
-const map = new ol.Map({
-    layers: [
-        new ol.layer.Tile({
-            source: new ol.source.XYZ({
-                url: 'http://localhost:8080/styles/klokantech-basic/{z}/{x}/{y}.png',
-                minZoom: 12,
-                maxZoom: 20,
-                tilePixelRatio: 2
-            })
-        })
-    ],
-    target: 'map',
-    view: view
-});
-
-const geolocation = new ol.Geolocation({
-    trackingOptions: {
+const geolocateControl = new mapboxgl.GeolocateControl({
+    positionOptions: {
         enableHighAccuracy: true
-    },
-    projection: view.getProjection(),
-    tracking: true
-});
-
-geolocation.on('error', function(error) {
-    console.log('Geolokalisierung ist fehlgeschlagen: ' + error.message);
-});
-
-const pointStyle = new ol.style.Style({
-    image: new ol.style.Circle({
-        radius: 6,
-        fill: new ol.style.Fill({
-            color: '#3399CC'
-        }),
-        stroke: new ol.style.Stroke({
-            color: '#fff',
-            width: 2
-        })
-    })
-});
-
-const positionFeature = new ol.Feature();
-positionFeature.setStyle(pointStyle);
-
-geolocation.on('change:position', function() {
-    var coordinates = geolocation.getPosition();
-    positionFeature.setGeometry(coordinates ?
-        new ol.geom.Point(coordinates) : null);
-    map.getView().animate({
-        center: coordinates,
-        zoom: 16,
-        duration: 1000
-    });
-    geolocation.setTracking(false);
-});
-
-new ol.layer.Vector({
-    map: map,
-    source: new ol.source.Vector({
-        features: [positionFeature]
-    })
-});
-
-const iconStyle = new ol.style.Style({
-    image: new ol.style.Icon({
-        anchor: [0, 1],
-        anchorXUnits: 'fraction',
-        anchorYUnits: 'fraction',
-        src: 'sammelstelle.svg'
-    })
-});
-
-var sammelstellen = new ol.layer.Vector({
-    map: map,
-    source: new ol.source.Vector({
-        url: 'http://localhost:8000/wp-json/sammelstellen/v1/sammelstellen',
-        format: new ol.format.GeoJSON({
-            dataProjection: 'EPSG:4326',
-            featureProjection: 'EPSG:3857'
-        })
-    }),
-    style: pointStyle
-});
-
-var element = document.getElementById('popup');
-
-var popup = new ol.Overlay({
-    element: element,
-    positioning: 'bottom-left',
-    autoPan: true,
-    autoPanAnimation: {
-        duration: 200
     }
 });
-map.addOverlay(popup);
+map.addControl(geolocateControl);
 
-map.on('click', function(event) {
-    var feature = map.forEachFeatureAtPixel(event.pixel, (feature) => feature, {
-        layerFilter: (layer) => layer === sammelstellen
+map.on('load', function() {
+    map.addSource('sammelstellen', {
+        type: 'geojson',
+        data: 'http://localhost:8000/wp-json/sammelstellen/v1/sammelstellen'
     });
-    if (feature) {
-        var coordinates = feature.getGeometry().getCoordinates();
-        popup.setPosition(coordinates);
-        element.innerHTML =
-            '<h5>' + feature.get('name') + '</h5>' +
-            '<p>' + feature.get('adresse') + '</p>' +
-            '<p><a href="' + feature.get('website') + '">Website</a></p>';
-    } else {
-        popup.setPosition(undefined);
-    }
+    map.addLayer({
+        id: 'sammelstellen',
+        type: 'circle',
+        source: 'sammelstellen'
+    });
+    geolocateControl.trigger();
+});
+
+map.on('mouseenter', 'sammelstellen', function() {
+    map.getCanvas().style.cursor = 'pointer';
+});
+
+map.on('mouseleave', 'sammelstellen', function() {
+    map.getCanvas().style.cursor = '';
+});
+
+map.on('click', 'sammelstellen', function(e) {
+    const coordinates = e.features[0].geometry.coordinates.slice();
+    const name = e.features[0].properties.name;
+    const adresse = e.features[0].properties.adresse;
+
+    new mapboxgl.Popup({
+        className: 'SammelstellePopup',
+        maxWidth: 'none'
+    })
+        .setLngLat(coordinates)
+        .setHTML('<h1>' + name + '</h1><p>' + adresse + '</p>')
+        .addTo(map);
 });
--- class.sammelstellen-shortcodes.php
+++ class.sammelstellen-shortcodes.php
@@ -8,21 +8,25 @@ class Sammelstellen_Shortcodes
 
         add_shortcode( 'sammelstellen-map', array( 'Sammelstellen_Shortcodes', 'map_shortcode' ) );
 
+        wp_register_script( 'mapbox-gl.js', plugin_dir_url( __FILE__ ) . '_inc/mapbox-gl.js' );
         wp_register_script( 'ol.js', plugin_dir_url( __FILE__ ) . '_inc/ol.js' );
         wp_register_script( 'map.js', plugin_dir_url( __FILE__ ) . '_inc/map.js' );
 
+        wp_register_style( 'mapbox-gl.css', plugin_dir_url( __FILE__ ) . '_inc/mapbox-gl.css' );
         wp_register_style( 'ol.css', plugin_dir_url( __FILE__ ) . '_inc/ol.css' );
         wp_register_style( 'map.css', plugin_dir_url( __FILE__ ) . '_inc/map.css' );
     }
 
     public static function map_shortcode( $atts = [], $content = null ) {
 
+        wp_enqueue_style( 'mapbox-gl.css' );
         wp_enqueue_style( 'ol.css' );
         wp_enqueue_style( 'map.css' );
+        wp_enqueue_script( 'mapbox-gl.js' );
         wp_enqueue_script( 'ol.js' );
         wp_enqueue_script( 'map.js' );
 
-        return '<div id="map" class="map"></div><div id="popup" class="Popup"></div>';
+        return '<div id="map" class="map"></div>';
     }
 
 }
