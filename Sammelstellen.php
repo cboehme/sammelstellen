e7c3fc80dd1f13caeacf843a5e7fe812db0b500e
Christoph Böhme <christoph@b3e.net>
2020-04-25 17:49:20 +0200

    Bump to version 1.1

--- Sammelstellen.php
+++ Sammelstellen.php
@@ -4,7 +4,7 @@
 Plugin Name: Sammelstellen
 Plugin URI: https://www.radentscheid-bonn.de/
 Description: Eine Sammelstellenliste für die Radentscheid Bonn Website
-Version: 1.0
+Version: 1.1
 Author: Christoph Böhme
 Author URI: https://b3e.net/
 License: GPL2
