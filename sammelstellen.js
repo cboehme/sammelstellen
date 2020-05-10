950c1d5ce5254cb7ed7cb68b5f88b2cc1ab183d9
Christoph Böhme <christoph@b3e.net>
2020-05-10 16:52:23 +0200

    Add switch Liste/Karte switch on small screens

--- sammelstellen.js
+++ sammelstellen.js
@@ -1,5 +1,5 @@
 import {html} from "htm/preact";
-import {useEffect, useState} from "preact/hooks";
+import {useEffect, useRef, useState} from "preact/hooks";
 
 import SammelstellenListe from "./sammelstellen-liste";
 import SammelstellenKarte from "./sammelstellen-karte";
@@ -9,10 +9,33 @@ export default function Sammelstellen({mapStyle, shrinkAt = Number.MAX_VALUE}) {
     const [sammelstellen, setSammelstellen] = useState({"type": "FeatureCollection", "features": []});
     useEffect(() => loadSammelstellen().then(setSammelstellen), []);
 
-    const [mapWidth, setMapWidth] = useState("100%");
+    const [mapWidth, setMapWidth] = useState(100);
     useEffect(() => {
         window.addEventListener("scroll", computeMapWidth);
         return () => window.removeEventListener("scroll", computeMapWidth);
+    }, []);
+
+    const [showingMap, setShowingMap] = useState(true);
+    const [showingList, setShowingList] = useState(true);
+    const mediaMaxWidth = useRef();
+    useEffect(() => {
+        mediaMaxWidth.current = window.matchMedia("(max-width: 41em)"); /*c*/
+        mediaMaxWidth.current.addListener(() => {
+            if (!mediaMaxWidth.current.matches) {
+                setShowingMap(true);
+                setShowingList(true);
+                computeMapWidth();
+            } else {
+                if (window.scrollY > shrinkAt) {
+                    setShowingMap(false);
+                    setShowingList(true);
+                } else {
+                    setShowingMap(true);
+                    setShowingList(false);
+                }
+            }
+        });
+        return () => mediaMaxWidth.current = null;
     }, []);
 
     const [selected, setSelected] = useState("");
@@ -25,11 +48,15 @@ export default function Sammelstellen({mapStyle, shrinkAt = Number.MAX_VALUE}) {
               top: 5em /*c*/;  
               height: 4em;
               text-align: center;
+              background-color: white;
             }
             .Map {
                 position: sticky;
                 top: 5em /*c*/;
                 height: calc(95vh - 5em /*c*/);
+            }
+            .List {
+                width: 50%;
             }
             
             @media(max-width: 41em /*c*/) {
@@ -40,15 +67,18 @@ export default function Sammelstellen({mapStyle, shrinkAt = Number.MAX_VALUE}) {
                     top: calc(5em /*c*/ + 4em);
                     height: calc(95vh - 5em /*c*/ - 4em);
                 }
+                .List {
+                    width: 100%;
+                }
             }
         </style>
-        <nav class="Switcher"><button>Kartenansicht</button></nav>
-        <div class="Map" style="margin-left: calc(100% - ${mapWidth}); width: ${mapWidth};">
+        <nav class="Switcher"><button onclick="${toggleMap}">Kartenansicht</button></nav>
+        <div class="Map" style="margin-left: calc(100% - ${mapWidth}%); width: ${mapWidth}%; display: ${showingMap ? 'block' : 'none'};">
             <${SammelstellenKarte} mapStyle=${mapStyle} 
                                    sammelstellen="${sammelstellen}" 
                                    selected="${selected}"/>
         </div>
-        <div style="width: 50%">
+        <div class="List" style="visibility: ${showingList ? 'visible' : 'hidden'};">
             <${SammelstellenListe} sammelstellen="${sammelstellen}" 
                                    onSammelstelleClick="${(id) => setSelected(id)}"/>
         </div>`;
@@ -56,18 +86,22 @@ export default function Sammelstellen({mapStyle, shrinkAt = Number.MAX_VALUE}) {
     function computeMapWidth() {
 
         const scrollTop = window.scrollY;
-        if (scrollTop > shrinkAt) {
+        if (scrollTop > shrinkAt && !mediaMaxWidth.current.matches) {
             let shrinkProgress = scrollTop - shrinkAt;
             if (shrinkProgress > 400) {
                 shrinkProgress = 400;
             }
             const newWidth = 100 - (50 / 400 * shrinkProgress);
-            setMapWidth(newWidth + "%");
+            setMapWidth(newWidth);
         } else {
-            setMapWidth("100%");
+            setMapWidth(100);
         }
     }
 
+    function toggleMap() {
+        setShowingMap(!showingMap);
+        setShowingList(!showingList);
+    }
 }
 
 function loadSammelstellen() {
