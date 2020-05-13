9079f798d592145f90814f41b5ce01ca64cd361e
Christoph Böhme <christoph@b3e.net>
2020-05-13 23:46:54 +0200

    Add "build" script for wordpress plugin

---
+++ pack-sammelstellen.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+ZIPFILE="sammelstellen-${1}.zip"
+
+rm -f $ZIPFILE
+ln -sf wordpress-plugin Sammelstellen
+zip -r $ZIPFILE Sammelstellen -x Sammelstellen/.git/\* Sammelstellen/.idea/\*
+rm Sammelstellen
