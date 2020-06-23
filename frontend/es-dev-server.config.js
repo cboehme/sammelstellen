81729b5149ee61f106cbc8b8a653f59e653ed7ea
Christoph Böhme <christoph@b3e.net>
2020-06-23 21:54:26 +0200

    Replace proxied REST call with static file
    The proxied REST call to th wordpress site does no longer work for
    retrieving the list of Sammelstellen because wordpress requires a nonce
    to be sent with the request. As we serve the Javascripts not through
    wordpress during development we do not have the nonce value.

--- frontend/es-dev-server.config.js
+++ frontend/es-dev-server.config.js
@@ -1,5 +1,3 @@
-const proxy = require("koa-proxies");
-
 module.exports = {
     port: 3000,
     hostname: "localhost",
@@ -9,10 +7,6 @@ module.exports = {
     preserveSymlinks: true,
     appIndex: "index.html",
     middlewares: [
-        proxy("/wp-json/", {
-            changeOrigin: true,
-            target: "https://www.radentscheid-bonn.de/"
-        }),
         (context, next) => {
             if (context.url === "/mapbox-gl.css") {
                 context.url = "node_modules/mapbox-gl/dist/mapbox-gl.css";
